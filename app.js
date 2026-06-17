48cf5a2e2306acc3ba58496a6406317f79d3c94c
Simeon Rediger <simeonrediger@gmail.com>
2026-06-17 18:39:15 -0400

    Start server

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Listening on port ${PORT}...`);
+});
