9d06543af260b4ff80ad58f77d1e1d46874942bb
Simeon Rediger <simeonrediger@gmail.com>
2026-06-17 18:47:01 -0400

    Add initial messages

--- routes/index-router.js
+++ routes/index-router.js
@@ -2,4 +2,17 @@ import { Router } from 'express';
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: 'Hi, there!',
+    user: 'Kevin',
+    added: new Date(),
+  },
+  {
+    text: 'Hello, world!',
+    user: 'Jennifer',
+    added: new Date(),
+  },
+];
+
 export default indexRouter;
