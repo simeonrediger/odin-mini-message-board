0f12e4270b6aa9acb4cf19ba00b4d11203ecb60c
Simeon Rediger <simeonrediger@gmail.com>
2026-06-21 01:35:33 -0400

    Fix message times by having client format them

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
+document.querySelectorAll('[data-time]').forEach(element => {
+  element.textContent = new Date(element.dataset.time).toLocaleString();
+});
+
 document
   .querySelectorAll('[data-scroll-to-bottom]')
   .forEach(element => (element.scrollTop = element.scrollHeight));
