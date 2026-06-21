f69b5c551c42ba15a74af38bd90e306ea918b227
Simeon Rediger <simeonrediger@gmail.com>
2026-06-21 14:25:33 -0400

    Extract behaviors into functions

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
-document.querySelectorAll('[data-time]').forEach(element => {
-  element.textContent = new Date(element.dataset.time).toLocaleString();
-});
+localizeTimes();
+scrollElementsToBottom();
 
-document
-  .querySelectorAll('[data-scroll-to-bottom]')
-  .forEach(element => (element.scrollTop = element.scrollHeight));
+function localizeTimes() {
+  document.querySelectorAll('[data-time]').forEach(element => {
+    element.textContent = new Date(element.dataset.time).toLocaleString();
+  });
+}
+
+function scrollElementsToBottom() {
+  document
+    .querySelectorAll('[data-scroll-to-bottom]')
+    .forEach(element => (element.scrollTop = element.scrollHeight));
+}
