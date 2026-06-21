55bd76fbc194d8abcf2a82bfeb04a4cba1bfbf59
Simeon Rediger <simeonrediger@gmail.com>
2026-06-20 20:22:53 -0400

    Group /new routes

--- routes/messages-router.js
+++ routes/messages-router.js
@@ -5,8 +5,10 @@ import * as messagesController from '../controllers/messages-controller.js';
 const messagesRouter = Router();
 
 messagesRouter.get('/', messagesController.getMessageBoard);
-messagesRouter.get('/new', messagesController.getNewMessageForm);
-messagesRouter.post('/new', messagesController.postNewMessageForm);
+messagesRouter
+  .route('/new')
+  .get(messagesController.getNewMessageForm)
+  .post(messagesController.postNewMessageForm);
 messagesRouter.get('/message/:messageId', messagesController.getMessageDetails);
 messagesRouter.get('/{*splat}', messagesController.getNotFoundPage);
 
